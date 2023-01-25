2d8e58ffd49b7eb345c103b5598c1c72ce4cad1e
jmonsellier <jmonsellier@gmail.com>
2023-01-25 14:47:37 +0100

    Ajout de commentaire et espacement

--- quiz.py
+++ quiz.py
@@ -17,13 +17,15 @@ while q < 3: # on boucle pour répondre à nos 3 questions
     while question != questions[q][1]: # on récupère la réponse dans la liste questions
         nb_de_chances -= 1 # on retire une chance suite à une mauvaise réponse
         print("Dommage ! Il te reste {} chances".format(nb_de_chances))
-        if nb_de_chances == 0:
+        if nb_de_chances == 0: # si plus aucune chance c'est perdu et on arrête le quiz
             print("Oh non ! Tu as perdu le jeu...")
             perdu=True
             break
         question = input(questions[q][0])
-    if perdu: break # si perdu on arrête le quizz
-    q += 1
+
+    if perdu: break # si perdu on arrête le quiz
+    
+    q += 1 # on passe à la question suivante
 
 if nb_de_chances > 0 and perdu==False:
     print("Bravo ! Tu as gagné le quiz !")
