a06aa92e4398aff4114309d0ed84ef68fd3f929e
jmonsellier <jmonsellier@gmail.com>
2023-01-25 14:45:10 +0100

    Ajout de commentaire

--- quiz.py
+++ quiz.py
@@ -1,3 +1,4 @@
+# on itialise le nombre de chance
 nb_de_chances = 3
 
 # on liste nos questions
@@ -14,14 +15,14 @@ perdu=False
 while q < 3: # on boucle pour répondre à nos 3 questions
     question= input(questions[q][0]) # on récupère la question dans la liste questions
     while question != questions[q][1]: # on récupère la réponse dans la liste questions
-        nb_de_chances -= 1
+        nb_de_chances -= 1 # on retire une chance suite à une mauvaise réponse
         print("Dommage ! Il te reste {} chances".format(nb_de_chances))
         if nb_de_chances == 0:
             print("Oh non ! Tu as perdu le jeu...")
             perdu=True
             break
         question = input(questions[q][0])
-    if perdu: break
+    if perdu: break # si perdu on arrête le quizz
     q += 1
 
 if nb_de_chances > 0 and perdu==False:
